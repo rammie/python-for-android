aecc1a743889c9fccdc481be5497f6b479209876
Ram Mehta <ram.mehta@gmail.com>
2017-07-01 03:18:16 -0400

    Use SDL2 mixer in ffpyplayer recipe.

--- pythonforandroid/recipes/ffpyplayer/__init__.py
+++ pythonforandroid/recipes/ffpyplayer/__init__.py
@@ -7,14 +7,16 @@ import os
 
 
 class FFPyPlayerRecipe(CythonRecipe):
-    version = 'master'
-    url = 'https://github.com/matham/ffpyplayer/archive/{version}.zip'
-    depends = ['python2', 'sdl2', 'ffmpeg']
+    version = 'android_sdl_mixer'
+    url = 'https://github.com/rammie/ffpyplayer/archive/{version}.zip'
+    depends = ['python2', 'sdl2', 'sdl2_mixer', 'ffmpeg']
     opt_depends = ['openssl', 'ffpyplayer_codecs']
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
         env = super(FFPyPlayerRecipe, self).get_recipe_env(arch)
 
+        env["USE_SDL2_MIXER"] = "1"
+        env["SDL_MIXER_INCLUDE_DIR"] = join(self.ctx.bootstrap.build_dir, 'jni', 'SDL2_mixer')
         env["SDL_INCLUDE_DIR"] = join(self.ctx.bootstrap.build_dir, 'jni', 'SDL', 'include')
         env["SDL_LIB_DIR"] = join(self.ctx.bootstrap.build_dir, 'libs', arch.arch)
 
